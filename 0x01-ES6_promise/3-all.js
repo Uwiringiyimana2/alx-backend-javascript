ea29d5803b6d63e07b23052e94c3e1fea3d94450
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-06-26 11:51:20 -0700

    5-photo-reject.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise
-  .all([uploadPhoto(), createUser()])
-  .then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`))
-  .catch(new Error('Signup system offline'));
+    .all([uploadPhoto(), createUser()])
+    .then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`))
+    .catch(new Error('Signup system offline'));
 }
