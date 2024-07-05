144073e19f021a4ce603efedf35f80f9b140d92d
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-07-05 08:59:54 -0700

    task_1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,41 @@ export interface Teacher {
   location: string;
   [key: string]: any;
 }
+
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+export interface classConstructor {
+  new (firstName: string, lastName:string): StudentClassInterface;
+}
+
+export interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass {
+  firstName: string;
+ lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = firstName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
