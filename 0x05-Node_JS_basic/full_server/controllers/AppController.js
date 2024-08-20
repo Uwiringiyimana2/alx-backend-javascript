e4f060c4af95f4ecf5b4b3967b59b4f4946a9126
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2024-08-20 15:45:15 +0000

    full_server

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,13 @@
+/*
+ * contains miscellenious route controller
+ */
+
+
+class AppController {
+  static getHomepage (request, response) {
+    response.status(200).send('Hello Holberton School!');
+  };
+}
+
+export default  AppController;
+module.exports =  AppController;
